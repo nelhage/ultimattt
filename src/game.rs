ab6613b27e672a80dd8f4e34f4454904547639d5
Nelson Elhage <nelhage@nelhage.com>
2020-04-09 21:41:56 -0700

    Try switching to Zobrist hashing

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,7 @@
 mod display;
 pub mod notation;
 
+use std::hash::{Hash, Hasher};
 use std::vec::Vec;
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
@@ -21,6 +22,13 @@ impl Player {
         match self {
             Player::X => "X",
             Player::O => "O",
+        }
+    }
+
+    pub fn as_bit(&self) -> usize {
+        match self {
+            Player::X => 0,
+            Player::O => 1,
         }
     }
 }
@@ -152,7 +160,7 @@ impl Default for Unpacked {
     }
 }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct Row {
     // These are each a packed [u9; 3] containing a bitmask for the
     // respective player's states. The low bits store index 0.
@@ -160,7 +168,7 @@ struct Row {
     o: u32,
 }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub(in crate) struct Subboards {
     rows: [Row; 3],
 }
@@ -243,7 +251,7 @@ impl Default for Subboards {
     }
 }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub(in crate) struct GameStates {
     // (x: u9, o: u9, drawn: u9), LSB first
     bits: u32,
@@ -323,13 +331,14 @@ impl GameStates {
     }
 }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Game {
     pub(in crate) next_player: Player,
     pub(in crate) next_board: Option<u8>,
     pub(in crate) boards: Subboards,
     pub(in crate) game_states: GameStates,
     pub(in crate) overall_state: BoardState,
+    hash: u64,
 }
 
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
@@ -378,6 +387,175 @@ pub enum MoveError {
     GameOver,
 }
 
+struct Zobrist {
+    // squares[board][cell][player.as_bit()]
+    squares: [[[u64; 2]; 9]; 9],
+    // boards[board] = [tie, X, O]
+    boards: [[u64; 3]; 9],
+}
+
+impl Zobrist {
+    fn for_board(&self, board: usize, state: BoardState) -> u64 {
+        match state {
+            BoardState::InPlay => 0,
+            BoardState::Drawn => self.boards[board][0],
+            BoardState::Won(Player::X) => self.boards[board][1],
+            BoardState::Won(Player::O) => self.boards[board][2],
+        }
+    }
+}
+
+static ZOBRIST_TABLES: Zobrist = Zobrist {
+    squares: [
+        [
+            [3903901466010275279, 17629543429219029125],
+            [6560537333460717463, 7986740484246722560],
+            [13448444757883081222, 8862075095357973495],
+            [627530935268485925, 13618924972420855844],
+            [5658841495040586966, 10588138687755045676],
+            [13375152968863883301, 11412907888164986633],
+            [3720144703103354770, 754926789191194911],
+            [10153693656287081050, 7904381750307070847],
+            [5989228250479128887, 13415111613617292659],
+        ],
+        [
+            [12059086116899244332, 7451163809502394975],
+            [14996391777122309193, 10143908630119605033],
+            [10754802737566917926, 1088440516914160163],
+            [8386103541441791909, 12542817629116669989],
+            [238424499285833975, 9653130381207852254],
+            [9405888958724483071, 17126726561198328420],
+            [8179610268055778624, 6553341512026093135],
+            [15560361405764370159, 16746408262836215598],
+            [2644246315876859064, 2061237395763558784],
+        ],
+        [
+            [16124045022113393060, 14256613376501550053],
+            [14021379837533551897, 6802895348214381347],
+            [136544599174301899, 9168882058527334167],
+            [3011668097940964029, 10773388103513099196],
+            [4335971041406852442, 13718617498506983821],
+            [9218116851555331607, 10002078858339948169],
+            [3740883737439103451, 2027739035571163026],
+            [4671695329495669244, 10164495824027210026],
+            [2930388333517988396, 11791177830834100646],
+        ],
+        [
+            [12195637642012800609, 4818057183511173711],
+            [5746580025108558176, 8593898498462459111],
+            [6915933671817634910, 500010199411656409],
+            [17160765776457446381, 17499166974477630742],
+            [7531030024674436671, 14444020855268728990],
+            [14420961019011337767, 9953309611959569954],
+            [12755489774125999224, 13386131398488311337],
+            [10572301849303105890, 14245356919393356844],
+            [7749810252646487439, 405107600915105234],
+        ],
+        [
+            [4673941769004790044, 9912376699992536506],
+            [13772861075326472726, 1424635966634839591],
+            [10448224672211504257, 10375061792833773117],
+            [810972899958790168, 297139134030571911],
+            [3433409098337369845, 4634971037386423006],
+            [12850522352033757384, 6596947357672707500],
+            [13778958088203008837, 14423899773881843734],
+            [8812853012364468235, 14203791236973137482],
+            [4374649817425260023, 2753676664979010312],
+        ],
+        [
+            [986597567356751249, 15570697413009087723],
+            [15530147278100853657, 17421029532736007804],
+            [2527105967667774100, 11286137556684937497],
+            [304139287659296867, 7540826990568408152],
+            [10166453674469440684, 6099008868128817969],
+            [6622564022921803344, 9408288026298252096],
+            [8349288625820524788, 2373305270142202147],
+            [17564047435738048593, 234641463252374735],
+            [15764970752357614869, 11458980784115948641],
+        ],
+        [
+            [12818556854687771997, 156920060851728499],
+            [18274899741850289430, 9654313182984924307],
+            [12255631505909503829, 865491575069738653],
+            [17256558342343098752, 17955956082692613109],
+            [578210944565411855, 3600222308699921328],
+            [3031820988293496544, 302500152432294006],
+            [2747199606362250167, 9452470642626238316],
+            [7808060539223555787, 15468571447339663953],
+            [11158004304530322043, 16263219473716500958],
+        ],
+        [
+            [17244338066467202728, 9053834545123193093],
+            [11453274589915392600, 6495049422195869991],
+            [6318660622973812624, 7950284959269046255],
+            [1293341356545473800, 6354918147291769233],
+            [12027263893405586912, 2827004469857518461],
+            [2510899281429553955, 5677839733827651068],
+            [15477038650223642817, 3714445228525652981],
+            [14711424822796252471, 645043225026648688],
+            [14451820229844848118, 16433456985459467283],
+        ],
+        [
+            [10734409564733582363, 17888608904115112824],
+            [14963165677727213545, 13735647310550505811],
+            [10977874651871842400, 12388199409902683965],
+            [18402500573870520482, 17645806566650012533],
+            [7617244385618196477, 2403187083095025443],
+            [15534413895419439910, 15322806809198407270],
+            [6998107393372259430, 17484518793461397802],
+            [1805771003956062686, 15508142961429924035],
+            [14983629085325617298, 13988792144298142757],
+        ],
+    ],
+    boards: [
+        [
+            10569293916965141230,
+            11160423188805547020,
+            4152050228531721776,
+        ],
+        [
+            9445060042156387674,
+            17343938531420928522,
+            4567518214753125608,
+        ],
+        [
+            10044836178330282261,
+            6985788723661997541,
+            2957325009523696543,
+        ],
+        [
+            4262220721745283853,
+            9629117720159326359,
+            14144349062870192621,
+        ],
+        [
+            15701379455324056445,
+            13805334679043656525,
+            2244893216642584252,
+        ],
+        [
+            14647228938910723007,
+            1431351858910568268,
+            1852814477193620178,
+        ],
+        [
+            10516372127856826326,
+            12162076319882157197,
+            4528292767345948788,
+        ],
+        [
+            8820780412613772490,
+            11456186954002538683,
+            7988953696186528238,
+        ],
+        [
+            11349973509132415302,
+            8414190152471347161,
+            14547444142552861968,
+        ],
+    ],
+};
+
 impl Game {
     pub fn new() -> Game {
         Game {
@@ -386,24 +564,29 @@ impl Game {
             boards: Default::default(),
             game_states: Default::default(),
             overall_state: BoardState::InPlay,
+            hash: 0,
         }
     }
 
     pub fn pack(bits: &Unpacked) -> Game {
         let mut boards: Subboards = Default::default();
+        let mut hash: u64 = 0;
         for row in 0..3 {
             let mut x: u32 = 0;
             let mut o: u32 = 0;
             for col in 0..3 {
-                let board = &bits.boards[3 * row + col];
+                let bnum = 3 * row + col;
+                let board = &bits.boards[bnum];
                 for (i, cell) in board.0.iter().enumerate() {
                     let idx: u32 = 1 << (9 * col + i);
                     match cell {
                         CellState::Played(Player::X) => {
                             x |= idx;
+                            hash ^= ZOBRIST_TABLES.squares[bnum][i][Player::X.as_bit()];
                         }
                         CellState::Played(Player::O) => {
                             o |= idx;
+                            hash ^= ZOBRIST_TABLES.squares[bnum][i][Player::O.as_bit()];
                         }
                         CellState::Empty => (),
                     }
@@ -415,6 +598,7 @@ impl Game {
         let mut states: GameStates = Default::default();
         for i in 0..9 {
             states.set(i, bits.game_states[i]);
+            hash ^= ZOBRIST_TABLES.for_board(i, bits.game_states[i]);
         }
         Game {
             next_player: bits.next_player,
@@ -422,6 +606,7 @@ impl Game {
             boards: boards,
             game_states: states,
             overall_state: bits.overall_state,
+            hash: hash,
         }
     }
 
@@ -451,10 +636,10 @@ impl Game {
         }
 
         self.boards.set(m.board(), m.square(), self.next_player);
-        self.game_states.set(
-            m.board(),
-            self.boards.check_winner(m.board(), self.next_player),
-        );
+        self.hash ^= ZOBRIST_TABLES.squares[m.board()][m.square()][self.next_player.as_bit()];
+        let board_state = self.boards.check_winner(m.board(), self.next_player);
+        self.game_states.set(m.board(), board_state);
+        self.hash ^= ZOBRIST_TABLES.for_board(m.board(), board_state);
         if self.game_states.in_play(m.square()) {
             self.next_board = Some(m.square() as u8);
         } else {
@@ -491,6 +676,15 @@ impl Game {
 
     pub fn board_state(&self, board: usize) -> BoardState {
         self.game_states.at(board)
+    }
+}
+
+impl Hash for Game {
+    fn hash<H>(&self, h: &mut H)
+    where
+        H: Hasher,
+    {
+        h.write_u64(self.hash);
     }
 }
 
