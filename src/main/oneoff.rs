b9f2848ddba927f8bf8b0fd7122f3b47f7d37246
Nelson Elhage <nelhage@nelhage.com>
2020-06-13 16:44:23 -0700

    Take symmetry into consideration when counting.
    And fix a dumb bug

--- src/main/oneoff.rs
+++ src/main/oneoff.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
 use bit_set::BitSet;
 use packed_simd::u16x8;
-use std::time::Instant;
+// use std::time::Instant;
+
+mod symmetry;
 
 const WIN_MASKS_SIMD: u16x8 = u16x8::new(0x7, 0x38, 0x1c0, 0x49, 0x92, 0x124, 0x111, 0x54);
 
@@ -19,6 +21,10 @@ enum Winner {
 }
 
 impl Game {
+    fn new() -> Self {
+        Game { x: 0, o: 0 }
+    }
+
     fn winner(self) -> Winner {
         if (u16x8::splat(self.x) & WIN_MASKS_SIMD)
             .eq(WIN_MASKS_SIMD)
@@ -38,7 +44,24 @@ impl Game {
     }
 
     fn as_index(self) -> usize {
-        (self.x | (self.o << 9)) as usize
+        (self.x as usize) | ((self.o as usize) << 9)
+    }
+
+    fn from_bits(bits: usize) -> Self {
+        Self {
+            x: (bits & 0x1ff) as u16,
+            o: ((bits >> 9) & 0x1ff) as u16,
+        }
+    }
+
+    fn at(self, i: usize) -> char {
+        let bit = 1_u16 << i;
+        match (self.x & bit != 0, self.o & bit != 0) {
+            (true, false) => 'X',
+            (false, true) => 'O',
+            (false, false) => '.',
+            (true, true) => '#',
+        }
     }
 
     fn empty(self, idx: usize) -> bool {
@@ -68,6 +91,27 @@ impl Game {
             }
         }
     }
+    fn apply(self, perm: &symmetry::Permutation) -> Game {
+        Game {
+            x: perm.apply(self.x),
+            o: perm.apply(self.o),
+        }
+    }
+
+    fn to_str(self) -> String {
+        format!(
+            "{} {} {}\n{} {} {}\n{} {} {}\n",
+            self.at(0),
+            self.at(1),
+            self.at(2),
+            self.at(3),
+            self.at(4),
+            self.at(5),
+            self.at(6),
+            self.at(7),
+            self.at(8),
+        )
+    }
 }
 
 #[derive(Default, Clone, Copy)]
@@ -78,16 +122,33 @@ struct Counts {
 }
 
 struct Brute {
+    do_symmetry: bool,
+    total: usize,
     stats: [[Counts; 8]; 8],
     seen: bit_set::BitSet,
 }
 
 impl Brute {
     fn search(&mut self, g: Game) {
-        if self.seen.contains(g.as_index()) {
-            return;
+        if self.do_symmetry {
+            if self.seen.contains(g.as_index()) {
+                return;
+            }
+            for perm in symmetry::PERM_TABLES.iter() {
+                self.seen.insert(g.apply(&perm).as_index());
+                /*
+                if self.seen.contains(g.permute(&perm).as_index()) {
+                    return;
+                }*/
+            }
+        } else {
+            if self.seen.contains(g.as_index()) {
+                return;
+            }
+            self.seen.insert(g.as_index());
         }
-        self.seen.insert(g.as_index());
+
+        self.total += 1;
         let stats = &mut self.stats[g.count_x()][g.count_o()];
         stats.visited += 1;
         match g.winner() {
@@ -115,30 +176,41 @@ impl Brute {
 }
 
 fn main() {
-    let mut brute = Brute {
+    let mut brute_symm = Brute {
+        do_symmetry: true,
         stats: Default::default(),
         seen: BitSet::new(),
+        total: 0,
     };
-    let t = Instant::now();
-    brute.search(Game { x: 0, o: 0 });
-    let e = Instant::now().duration_since(t);
-    println!("time:     {}.{:03}", e.as_secs(), e.subsec_millis());
-    let mut total = 0;
-    for (x, row) in brute.stats.iter().enumerate() {
+    brute_symm.search(Game { x: 0, o: 0 });
+
+    println!(
+        "reachable boards(modulo symmetry): {} total={}",
+        brute_symm.total,
+        brute_symm.seen.len()
+    );
+
+    let mut brute_raw = Brute {
+        do_symmetry: false,
+        stats: Default::default(),
+        seen: BitSet::new(),
+        total: 0,
+    };
+    brute_raw.search(Game { x: 0, o: 0 });
+
+    println!(
+        "reachable boards(ignore symmetry): {} total={}",
+        brute_raw.total,
+        brute_raw.seen.len()
+    );
+
+    println!();
+
+    for (x, row) in brute_symm.stats.iter().enumerate() {
         print!("x={}", x);
         for (_o, e) in row.iter().enumerate() {
             print!("  {:3}/{:-3}/{:3}|", e.x, e.o, e.visited);
-            total += e.visited;
         }
         println!();
-        /*
-        let (x, o, v) = e
-            .iter()
-            .fold((0, 0, 0), |(x, o, v), r| (x + r.x, o + r.o, v + r.visited));
-
-        println!("{}  x:{:4} o:{:4} / {:4}", i, x, o, v);
-        */
     }
-
-    println!("reachable boards: {}", total);
 }
